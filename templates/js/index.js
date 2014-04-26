36fe561955a69a5b7e4fdb5186b166c08379414e
Phil Snyder <subfivemile@gmail.com>
2014-04-26 01:10:13 -0700

    Div fixes

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 	$friends = $('#friendlist li');
 	$friends.filter(':even').css('background', 'gray');
-	$friends.filter(':odd').css('background', 'black')
+	$friends.filter(':odd').css('background', 'black');
+	var login = false;
+	$friendlist = $('#friendlist');
+	if (login) {
+		$friendlist.css('background', '');
+		$friendlist.css('text-align', 'left');
+		$friendlist.css('color', 'white');
+		$friendlist.css('padding-top', '0px');
+	};
+	$friendlist.click(function() {
+		friendlist_toggle();
+	});
 });
+
+function friendlist_toggle() {
+	$friendlist.css('background', '');
+	$friendlist.css('text-align', 'left');
+	$friendlist.css('color', 'white');
+	$friendlist.css('padding-top', '0px');
+	// do fancy stuff
+}
