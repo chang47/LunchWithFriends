e44d8c27198d81d207b26cfdcaa1b3295da125ab
Phil Snyder <subfivemile@gmail.com>
2014-04-26 05:25:40 -0700

    small info boxes next to name

--- templates/js/index.js
+++ templates/js/index.js
@@ -4,10 +4,18 @@ $(document).ready(function() {
 	request("get-friends",{}, function(data) {
 		list = $("#friendlist ul");
 		list.html("");
+		var time;
 		$.each(data, function(k, person) {
-			list.append("<li><img src='" + person.dp + "' />    " + person.name + "</li>");
+			starttime = parseTime(person.startdatetime);
+			endttime = parseTime(person.enddatetime);
+			list.append("<li><img src='" + person.dp + "' />    <div class='smallinfo'>Start:" + starttime + "<br /> End:" + endtime + "</div>" + person.name + "</li>");
 			lunchLine.push(person);
 		});
 	})
 });
+
+function parseTime(time) {
+	var temp = time.split(" ")[1].split(":");
+	return Math.floor(time[0]) + ":" + time[1];
+}
 
